967b8c830e72f93992fc25c0f6741a546754e48b
chica25 <patricia@patriciaco.com>
2021-04-14 01:15:28 -0400

    Edit functions, css styling and html code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const slide = () => {
-    const burger = document.querySelector('.burger');
+    const burger = document.querySelector('.menu-bar');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
 
@@ -8,9 +8,9 @@ burger.addEventListener('click', () => {
 
     navLinks.forEach((link, index) => {
             if(link.style.animation){
-                link.style.animation = '';
+                link.style.animation = ''
             } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0 }s`;
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3 }s`;
             }
         });
 
